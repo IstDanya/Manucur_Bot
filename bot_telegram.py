485b66ae4ce5a9b96aa020527410be6948643620
Danya <ichbindanya@yandex.ru>
2023-02-26 20:11:19 +0300

    first commit

---
+++ bot_telegram.py
@@ -0,0 +1,20 @@
+from aiogram import Bot, types, Dispatcher
+from aiogram.utils import executor
+
+import os 
+
+bot = Bot(token=os.environ['TOKEN'])
+
+dp = Dispatcher(bot)
+
+@dp.message_handler(commands=['start'])
+# @dp.message_handler()
+async def echo_send(message : types.Message):
+	await message.reply('Приветики, мб НОГОТОЧКИ?(метод reply, ответ на сообщение)')
+	# await message.answer('Метод answer')
+	await bot.send_message("ПНХ")
+
+
+
+
+executor.start_polling(dp, skip_updates=True)
---
+++ run_bot.bat
@@ -0,0 +1,10 @@
+@echo off
+
+call "%~dp0venv\Scripts\activate"
+
+set TOKEN=6040603306:AAHEdXRCYKYmvB1tHHYaW95DInKUQbrC-SM
+
+python bot_telegram.py
+
+pause
+
