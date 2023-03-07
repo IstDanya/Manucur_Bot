bc64867e468f9a3141025fd1dd0f730f4577ec2c
Danya <ichbindanya@yandex.ru>
2023-03-07 21:55:22 +0300

    Test_07.03.2023

--- bot_telegram.py
+++ bot_telegram.py
@@ -3,17 +3,40 @@ from aiogram.utils import executor
 
 import os 
 
+# entry point
 bot = Bot(token=os.environ['TOKEN'])
-
 dp = Dispatcher(bot)
 
-@dp.message_handler(commands=['start'])
-# @dp.message_handler()
+
+"""***************************USER PART***************************** """
+@dp.message_handler(commands=['start', 'help'])
+async def command_start(message : types.Message):
+	try:
+		await bot.send_message(message.from_user.id, "Приятного когтеточения)")
+		await message.delete()
+	except:
+		await message.reply('Бот еще не знает о вас, познакомтесь с ним: \nhttps://t.me/ManucurBot')
+
+@dp.message_handler(commands=['Режим_работы'])
+async def operating_mode(message : types.Message):
+	await bot.send_message(message.from_user.id, "Когда Alexa нет дома")
+
+@dp.message_handler(commands=['Адрес'])
+async def work_place(message : types.Message):
+	await bot.send_message(message.from_user.id, 'ул. Трудовиков 28')
+
+
+
+
+"""***************************ADMIN PART**************************** """
+"""***************************COMMON PART*************************** """
+
+@dp.message_handler()
 async def echo_send(message : types.Message):
-	await message.reply('Приветики, мб НОГОТОЧКИ?(метод reply, ответ на сообщение)')
-	# await message.answer('Метод answer')
-	await bot.send_message("ПНХ")
-	# сделал modify
+	if message.text == "hi":
+		await message.answer("Hello, Ich heise ManucurBot")
+	
+	
 
 
 
